7c67167edb4e7364c533e64ea0a1d06091f36b44
doaa-altarawy <doaa.altarawy@gmail.com>
2017-08-02 09:58:16 -0400

    Implemented pass and advisor function issue2

--- student.py
+++ student.py
@@ -6,10 +6,11 @@ A module for handling school students related information
 class Student(object):
     """Basic student class"""
 
-    def __init__(self, name, ID, GPA):
+    def __init__(self, name, ID, GPA, status):
         self.name = name
         self._GPA = GPA     # protected
         self.__ID = ID      # private
+        self.status = status
 
     def get_GPA(self):
         return self._GPA
@@ -19,3 +20,27 @@ class Student(object):
 
     def __str__(self):
         return "Student Name " + self.name
+
+
+class GradStudent(Student):
+
+    def __init__(self, name, ID, GPA):
+        super().__init__(name, ID, GPA, 'Grad')
+
+    def is_pass(self):
+        return self.get_GPA() >= 3
+
+    def set_advisor(self, advisor):
+        self.advisor = advisor
+
+    def get_advisor(self):
+        return self.advisor
+
+
+class UndergradStudent(Student):
+
+    def __init__(self, name, ID, GPA):
+        super().__init__(name, ID, GPA, 'Undergrad')
+
+    def is_pass(self):
+        return self.get_GPA() >= 2.5
